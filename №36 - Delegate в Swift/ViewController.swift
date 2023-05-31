252cb3040830188b10cf54002f61190d0fa13e26
TheViLkiS <thevilkis@gmail.com>
2023-05-31 13:36:36 +0400

    Initial Commit

---
+++ №36 - Delegate в Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  №36 - Delegate в Swift
+//
+//  Created by Дмитрий Гусев on 31.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
